5f9335122ccd587c08a4f7c276eb47b94accea51
Cezary Grzanka <cezarygrzanka@gmail.com>
2019-02-08 16:32:36 +0100

    Change table to dictionary in exam list

--- api/classes/exams.py
+++ api/classes/exams.py
@@ -2,8 +2,10 @@ from flask import jsonify, request
 from flask_restful import Resource
 import functions as fn
 
+
 # Return list of exams
 class ExamsList(Resource):
     def get(self):
         data = fn.getExamsList()
+        data = {'list': data}
         return data, 200
