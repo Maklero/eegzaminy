9549bf7a6ec76fd9b2b686ddba0a3041697c038f
Cezary Grzanka <cezarygrzanka@gmail.com>
2019-02-11 23:57:37 +0100

    Fix cors policy

--- api/app.py
+++ api/app.py
@@ -1,10 +1,23 @@
 from flask import Flask, jsonify
 from flask_restful import Api
+from flask_cors import CORS
 
 import classes.exams as exams
 
 app = Flask(__name__)
 api = Api(app)
+cors = CORS(app, resources={
+    r"/v1/*": {
+        "origins": [
+            "http://localhost",
+            "http://127.0.0.1:*",
+            "http://192.168.8.*",
+            "http://192.168.8.*:*",
+            "https://localhost",
+            "https://*.eegzaminy.pl"
+        ]
+    }
+})
 
 
 # Static Welcome route
