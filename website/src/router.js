9e9c61c6ce7c93d94e29b3ca53e5e3c5955d416d
Cezary Grzanka <cezarygrzanka@gmail.com>
2019-02-15 22:32:49 +0100

    Add Exam.vue component

--- website/src/router.js
+++ website/src/router.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import Router from 'vue-router';
 
 import Home from './views/Home.vue';
+import Exam from './views/Exam.vue';
+
 
 Vue.use(Router);
 
@@ -22,6 +24,7 @@ export default new Router({
     {
       path: '/exam/:name',
       name: 'exam',
+      component: Exam,
     },
   ],
 });
---
+++ website/src/views/Exam.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="exam">
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'Exam',
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
