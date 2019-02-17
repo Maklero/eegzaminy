c243f6edc03531fc9084d23758b5b21d44ab33b0
Cezary Grzanka <cezarygrzanka@gmail.com>
2019-02-17 01:37:18 +0100

    Add question generating

---
+++ website/src/components/Question.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="question-container">
+    <div class="question">
+      <h3>Pytanie {{ index + 1 }}</h3>
+      <span>{{ question.question }}</span>
+    </div>
+    <div v-if="question.img">
+      <img :src="question.img" :alt="question.img">
+    </div>
+    <div v-for="(label, index) in names" class="answer">
+      <div class="answer-name">
+        <input
+          type="radio"
+          :name="question.id"
+          :value="label"
+          :id="`o${index}_${question.id}`"
+        >
+      </div>
+      <div class="answer-content">
+        <label :for="`o${index}_${question.id}`">
+          {{ label }}. {{ question[label] }}
+        </label>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Question',
+  props: {
+    question: {
+      type: Object,
+      required: true,
+    },
+    index: {
+      type: Number,
+      required: true,
+    },
+    names: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .question-container {
+    margin: 2px 0;
+    width: 98%;
+    min-height: 100px;
+    background-color: red;
+    overflow: hidden;
+
+    img {
+      width: 100%;
+    }
+  }
+</style>
--- website/src/views/Exam.vue
+++ website/src/views/Exam.vue
@@ -1,36 +1,68 @@
 <template>
-  <div class="exam">
-    {{ examData }}
+  <div class="exam-container">
+    <form class="exam">
+      <Question
+        v-for="(question, index) in examData.questions"
+        :names="examData.name"
+        :index="index"
+        :question="question"
+        :key="question.id"
+      />
+      <input type="hidden" :value="questionsNumbersList" name="list">
+      <input type="hidden" :value="examData.title" name="egzam_name"/>
+      <button type="button" id="button">Sprawdź</button>
+    </form>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
 import dataFile from '@/api.json';
+import Question from '@/components/Question.vue';
 
 const API = dataFile.url;
 
 export default {
   name: 'Exam',
+  components: { Question },
   data() {
     return {
       examName: null,
       examData: null,
+      questionsNumbersList: null,
     };
+  },
+  methods: {
+    getExam() {
+      axios.get(`${API}/exams/${this.examName}`)
+        .then((response) => {
+          this.examData = response.data;
+          this.questionsNumbersList = '';
+          response.data.questions.forEach((row) => {
+            this.questionsNumbersList += `${row.id};`;
+          });
+        })
+        .catch(error => console.log(error));
+    },
   },
   mounted() {
     this.examName = this.$route.params.name;
-    axios.get(`${API}/exams/${this.examName}`)
-      .then((response) => {
-        this.examData = response;
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    this.getExam();
+  },
+  watch: {
+    $route() {
+      this.examName = this.$route.params.name;
+      this.getExam();
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-
+  .exam {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 </style>
