7ee23b44ee54c735242b6121093241b930dd6ab3
Cezary Grzanka <cezarygrzanka@gmail.com>
2019-02-17 02:22:41 +0100

    Mobile view fnished

--- website/src/components/Question.vue
+++ website/src/components/Question.vue
@@ -8,19 +8,15 @@
       <img :src="question.img" :alt="question.img">
     </div>
     <div v-for="(label, index) in names" class="answer">
-      <div class="answer-name">
-        <input
-          type="radio"
-          :name="question.id"
-          :value="label"
-          :id="`o${index}_${question.id}`"
-        >
-      </div>
-      <div class="answer-content">
-        <label :for="`o${index}_${question.id}`">
-          {{ label }}. {{ question[label] }}
-        </label>
-      </div>
+      <input
+        type="radio"
+        :name="question.id"
+        :value="label"
+        :id="`o${index}_${question.id}`"
+      >
+      <label :for="`o${index}_${question.id}`">
+        {{ label }}. {{ question[label] }}
+      </label>
     </div>
   </div>
 </template>
@@ -47,14 +43,61 @@ export default {
 
 <style lang="scss" scoped>
   .question-container {
-    margin: 2px 0;
+    flex-direction: column;
+    margin: 4px 0;
     width: 98%;
     min-height: 100px;
-    background-color: red;
     overflow: hidden;
 
     img {
       width: 100%;
+    }
+  }
+
+  .question, .answer, span, .question-container {display: flex}
+
+  .question, .answer {
+    border-radius: 2px;
+    padding: 7px;
+  }
+
+  .question {
+    flex-direction: column;
+    min-height: 100px;
+    background-color: #90A4AE;
+
+    h3 {
+      margin: 0;
+      width: 100%;
+      height: 30px;
+    }
+
+    &, span {
+      align-items: center;
+      justify-content: center;
+    }
+  }
+
+  .answer {
+    padding: 0;
+    margin: 2px 0 0 0;
+    width: 100%;
+    min-height: 50px;
+    flex-direction: row;
+    align-items: center;
+    background-color: #455A64;
+
+    input[type=radio] {
+      margin: 0;
+      width: 40px;
+    }
+
+    label {
+      display: flex;
+      align-items: center;
+      width: 100%;
+      min-height: 50px;
+      padding: 5px;
     }
   }
 </style>
